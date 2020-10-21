2c974f984a1727d6a1e3f7f6a16f9e6ffd6caf8c
Conor Fox <conor@conorfox.com>
2020-10-21 17:45:20 +0100

    Single stack Tetris working

--- lab1/tetris.py
+++ lab1/tetris.py
@@ -1,24 +1,36 @@
 from stackA import *
 from random import randint
+from time import time
 
 def game():
     stack = Stack()
     blocks = []
+    score = 0
     for i in range(20):
         blocks.append(randint(0, 2))
     print(stack)
     # print(blocks)
-    while len(blocks) > 0:
+    while len(blocks) > 0 and stack.length() <= 5:
         next = blocks.pop()
-        print('Tetris (press \'q\' to quit)')
+        print('------------\nTetris (press \'q\' to quit)\n------------')
         print()
         print(stack)
         print('Next -> ', next)
+        start = time()
         choice = input('(y)es/(n)o: ')
-        if choice == 'y':
-            stack.push(next)
-        elif choice == 'q':
+        end = time()
+        if choice == 'q':
             break
+        elif (end - start) > 5 or choice == 'y':
+            if (end - start) > 5:
+                print('You took too long!')
+            if next == stack.top():
+                score += 1
+                stack.pop()
+            else:
+                stack.push(next)
+    else:
+        print('---------\nGame Over\nScore:', score, '\n---------')
 
 if __name__ == '__main__':
     game()
