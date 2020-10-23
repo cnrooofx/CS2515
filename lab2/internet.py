38f9d53a59249fed53ccdedfdc499a959091ba39
Conor Fox <conor@conorfox.com>
2020-10-23 18:26:12 +0100

    Start of internet

---
+++ lab2/internet.py
@@ -0,0 +1,74 @@
+
+from my_queue import Queue
+
+
+class Packet:
+    def __init__(self):
+        self._content = ''
+        self._address = None
+        self._sender = None
+        self._path = Queue()
+
+    @property
+    def content(self):
+        return self._content
+
+    @content.setter
+    def content(self, content):
+        if isinstance(content, str):
+            self._content = content
+        else:
+            raise TypeError("Content must be a string")
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, address):
+        if isinstance(address, Client):
+            self._address = address
+        else:
+            raise TypeError("Address must reference a Client object")
+
+    @property
+    def sender(self):
+        return self._sender
+
+    @sender.setter
+    def sender(self, sender):
+        if isinstance(sender, Client):
+            self._sender = sender
+        else:
+            raise TypeError("Sender must reference a Client object")
+
+    @property
+    def path(self):
+        return self._path
+
+    def updatePath(self, path_update):
+        self.path.append(path_update)
+
+
+class Client:
+    def __init__(self):
+        self._queue = Queue()
+
+    def send(self):
+        return
+
+    def recieve(self):
+        return
+
+
+class Router:
+    def __init__(self):
+        self._queue = Queue()
+
+    def process(self):
+        return
+
+
+pack = Packet()
+pack.content = 'hello'
+print(pack.content)
