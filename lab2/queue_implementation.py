6949162204ea27e43b6b0de4a58085d5cd2a3907
Conor Fox <conor@conorfox.com>
2020-10-23 12:58:42 +0100

    Queue reverser using a stack

---
+++ lab2/queue_implementation.py
@@ -0,0 +1,37 @@
+from queues import QueueV2 as Queue
+from stackA import Stack
+from random import randint
+
+
+def random_queue(length=20):
+    queue = Queue()
+    for _ in range(length):
+        queue.enqueue(randint(1, 10))
+    return queue
+
+
+def queue_reverser(queue):
+    new_queue = Queue()
+    stack = Stack()
+    length = queue.length()
+    for i in range(length):
+        item = queue.dequeue()
+        stack.push(item)
+        queue.enqueue(item)
+    for i in range(length):
+        new_queue.enqueue(stack.pop())
+    return new_queue
+
+
+def main():
+    print('Queue reverser\n')
+    q1 = random_queue(5)
+    print(q1)
+
+    q_rev = queue_reverser(q1)
+    print(q_rev)
+    print(q1)
+
+
+if __name__ == '__main__':
+    main()
