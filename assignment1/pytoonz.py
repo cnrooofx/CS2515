87201a5871cfa8cc99c9ca10d013c47ade6412e7
Conor Fox <conor@conorfox.com>
2020-10-30 18:27:43 +0000

    Basic methods and docstrings

--- assignment1/pytoonz.py
+++ assignment1/pytoonz.py
@@ -5,28 +5,125 @@ Author: Conor Fox 119322236
 """
 
 
+class DLNode:
+    """Node for an item in a Doubly Linked List."""
+
+    def __init__(self, item, prevnode, nextnode):
+        """Create a DLNode object."""
+        self.item = item
+        self.next = nextnode
+        self.prev = prevnode
+
+
 class Track:
+    """Track object to be used in a PyToonz playlist."""
+
     def __init__(self, name, artiste):
+        """Create a Track object."""
         self._name = name
         self._artiste = artiste
         self._timesplayed = 0
 
     def __str__(self):
+        """Return a string representation of the track."""
         s = "{}; {} ({})".format(self._name, self._artiste, self._timesplayed)
         return s
 
     def get_name(self):
+        """Return the name of the track."""
         return self._name
 
     def get_artiste(self):
+        """Return the name of the artiste."""
         return self._artiste
 
     def play(self):
+        """Play the track and increment it's play count."""
         self._timesplayed += 1
         return self
 
 
+class PyToonz:
+    """Class for a PyToonz playlist."""
+
+    def __init__(self):
+        """Create a PyToonz object."""
+        self._head = DLNode(None, None, None)
+        self._tail = DLNode(None, self._head, None)
+        self._head.next = self._tail
+        self._length = 0
+        self._current = self._head
+
+    def __str__(self):
+        """Return a string representation of the playlist."""
+        s = ''
+        return s
+
+    def length(self):
+        """Return the length of the playlist."""
+        return self._length
+
+    def add_track(self, track):
+        """Add track to the end of the playlist."""
+        self._add_node_after(track, self._tail.prev)
+        self._length += 1
+
+    def get_current(self):
+        """Return the currently selected track."""
+        if self._length == 0:
+            return None
+        return self._current.item
+
+    def add_after(self, track):
+        """Add a new track after the current track."""
+        self._add_track_node(track, self._current)
+
+    def next_track(self):
+        """Select the next track in the playlist."""
+        if self._current.next != self._tail:
+            self._current = self._current.next
+        else:
+            self._current = self._head.next
+
+    def prev_track(self):
+        """Select the previous track in the playlist."""
+        if self._current.next != self._head:
+            self._current = self._current.prev
+        else:
+            self._current = self._tail.prev
+
+    def reset(self):
+        """Remove all tracks from the playlist."""
+        pass
+
+    def play(self):
+        """Play the currently selected track."""
+        if self._current is not None:
+            self._current.play()
+
+    def remove_current(self):
+        """Remove the current track."""
+        before = self._current.prev  # Track after removed one
+        after = self._current.next   # Track before removed one
+        before.next = after          # Link the two tracks
+        after.prev = before
+        self._current.element = None  # Set the node at current to None
+        self._current.next = None
+        self._current.prev = None
+        self._current = after        # Set the current pointer to next track
+
+    def _add_track_node(self, track, before):
+        new_node = DLNode(track, None, None)  # Create new node object
+        after = before.next     # Get the next track in the playlist
+        new_node.next = after   # Link the new track into the playlist
+        after.prev = new_node
+        before.next = new_node
+        new_node.prev = before
+        self._length += 1       # Increment the playlist length
+
+
 def test():
+    """Test methods for PyToonz."""
     print("------------\nTrack\n------------")
     t1 = Track('Track 1', 'Artist 1')
     t2 = Track('Track 2', 'Artist 1')
