1d9f8356616f162cc97485b027a7ee87ad6ffc55
Conor Fox <conor@conorfox.com>
2020-11-06 12:23:09 +0000

    Fix remove track & reset, play now prints Track

--- assignment1/pytoonz.py
+++ assignment1/pytoonz.py
@@ -85,7 +85,7 @@ class PyToonz:
 
     def get_current(self):
         """Return the currently selected track."""
-        if self._length == 0:
+        if self._current is None:
             return None
         return 'Current track: ' + str(self._current.item)
 
@@ -113,15 +113,17 @@ class PyToonz:
             i = 0
             node = self._head.next
             while i < self._length:
+                next_node = node.next
                 self._remove_track_node(node)
-                node = node.next
+                node = next_node
                 i += 1
 
     def play(self):
         """Play the currently selected track."""
-        if self._length == 0:
-            return None
-        return self._current.item.play()
+        if self._current is None:
+            print('Error - No track currently selected.')
+        else:
+            print(self._current.item.play())
 
     def remove_current(self):
         """Remove the current track."""
@@ -144,19 +146,21 @@ class PyToonz:
     def _remove_track_node(self, track):
         before = track.prev  # Track before removed one
         after = track.next   # Track after removed one
-        before.next = after          # Link the two tracks
+
+        before.next = after          # Link the two tracks together
         after.prev = before
 
-        track.element = None  # Set the node at current to None
+        track.item = None  # Set the node at current to None
         track.next = None
         track.prev = None
 
         self._length -= 1
-        if self._length != 0:
-            if after != self._tail:
-                self._current = after
-            else:
-                self._current = before
+        if self._length == 0:  # If the list is now empty,
+            self._current = None  # Set the current to None
+        elif after == self._tail:  # If the item was the last in the list
+            self._current = before  # Set the current to the last item
+        else:
+            self._current = after
 
 
 def test():
@@ -168,10 +172,10 @@ def test():
     print(t1)
     print(t2)
     print(t3)
-    print(t1.play())
-    print(t1.play())
-    print(t1.play())
-    print(t1.play())
+    t1.play()
+    t1.play()
+    t1.play()
+    t1.play()
     print(t1)
     print(t2.get_artiste())
     print(t3.get_name())
@@ -194,7 +198,7 @@ def sample_test():
              Giants; Dermot Kennedy (0)
              Holy; Justin Bieber Ft Chance (0)""")
     print('-' * 25)
-    print(playlist.play())
+    playlist.play()
     print("""Should be:
              Playing: Looking for me; Paul Woolford and Diplo / Lomax (1)""")
     playlist.next_track()
@@ -213,7 +217,7 @@ def sample_test():
     playlist.add_track(t4)
     playlist.next_track()
     print('-' * 25)
-    print(playlist.play())
+    playlist.play()
     print('Should be:\n\tPlaying: Holy; Justin Bieber Ft Chance (1)')
     print('-' * 25)
     print(playlist)
