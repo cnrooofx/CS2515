c662a5e9c5404a929d14aa92e628488749eb17b0
Conor Fox <conor@conorfox.com>
2020-12-11 19:12:38 +0000

    Add test methods into movieLib file

--- assignment2/movieLib.py
+++ assignment2/movieLib.py
@@ -6,6 +6,7 @@ Author: Conor Fox 119322236
 
 from functools import total_ordering
 from bst import BSTNode
+from time import time
 
 
 @total_ordering
@@ -102,3 +103,166 @@ class MovieLib:
             title - the title of the movie to be removed
         """
         return self.bst.remove(Movie(title))
+
+    def _testadd():
+        library = MovieLib()
+        library.add("Memento", "11/10/2000", 113)
+        print(str(library))
+        print("> adding Melvin and Howard")
+        library.add("Melvin and Howard", "19/09/1980", 95)
+        print(str(library))
+        print("> adding a second version of Melvin and Howard")
+        library.add("Melvin and Howard", "21/03/2007", 112)
+        print(str(library))
+        print("> adding Mellow Mud")
+        library.add("Mellow Mud", "21/09/2016", 92)
+        print(str(library))
+        print("> adding Melody")
+        library.add("Melody", "21/03/2007", 113)
+        print(str(library))
+        return library
+
+    def _test():
+        library = MovieLib()
+        library.add("B", "b", 1)
+        print("Library:", library)
+        print("adding", "A")
+        library.add("A", "a", 1)
+        print("Library:", library)
+        print("removing", "A")
+        library.remove("A")
+        print("Library:", library)
+        print("adding", "C")
+        library.add("C", "c", 1)
+        print("Library:", library)
+        print("removing", "C")
+        library.remove("C")
+        print("Library:", library)
+        print("adding", "F")
+        library.add("F", "f", 1)
+        print("Library:", library)
+        print("removing", "B")
+        library.remove("B")
+        print("Library:", library)
+        print("adding", "C")
+        library.add("C", "c", 1)
+        print("Library:", library)
+        print("adding", "D")
+        library.add("D", "d", 1)
+        print("Library:", library)
+        print("adding", "C")
+        library.add("C", "c", 1)
+        print("Library:", library)
+        print("adding", "E")
+        library.add("E", "e", 1)
+        print("Library:", library)
+        print("removing", "B")
+        library.remove("B")
+        print("Library:", library)
+        print("removing", "D")
+        library.remove("D")
+        print("Library:", library)
+        print("removing", "C")
+        library.remove("C")
+        print("Library:", library)
+        print("removing", "E")
+        library.remove("E")
+        print("Library:", library)
+        print("adding", "L")
+        library.add("L", "l", 1)
+        print("Library:", library)
+        print("adding", "H")
+        library.add("H", "h", 1)
+        print("Library:", library)
+        print("adding", "I")
+        library.add("I", "i", 1)
+        print("Library:", library)
+        print("adding", "G")
+        library.add("G", "g", 1)
+        print("Library:", library)
+        print("removing", "L")
+        library.remove("L")
+        print("Library:", library)
+        print("removing", "H")
+        library.remove("H")
+        print("Library:", library)
+        print("removing", "I")
+        library.remove("I")
+        print("Library:", library)
+        print("removing", "G")
+        library.remove("G")
+        print("Library:", library)
+
+
+def build_library(filename):
+    """Return a library of Movie files built from filename."""
+    # open the file
+    file = open(filename, "r", encoding="utf8")
+
+    # create the library
+    library = MovieLib()
+
+    filecount = 0
+    count = 0
+
+    # now cycle through the  lines in the file, adding the movies to the
+    # library
+    for line in file:
+        filecount += 1
+        inputlist = line.split('\t')
+        added = library.add(inputlist[0], inputlist[1], inputlist[2])
+        if added is not None:
+            count += 1
+
+    # print out some info for sanity checking
+    print("read a file with", filecount, "movies")
+    print("Built a library with", count, "unique movie titles")
+    return library
+
+
+def main():
+    """Test methods."""
+    # MovieLib._testadd()
+    # print('++++++++++')
+    # MovieLib._test()
+
+    # file = "smallmovies.txt"
+    # file = "small_repeated_movies.txt"
+    file = "movies.txt"
+
+    before = time()
+    newlibrary = build_library(file)
+    after = time()
+    time_taken = after - before
+
+    print("\tSize:", newlibrary.bst.size())
+    print("\tTime to build: %fs" % time_taken)
+    print("\tHeight of root node:", newlibrary.bst.height())
+    print("\tProper BST?", newlibrary.bst._properBST())
+
+    print("Searching")
+
+    before = time()
+    movie = newlibrary.search("Iron Man")
+    after = time()
+    time_taken = after - before
+    print("\t%s found in %fs" % (movie, time_taken))
+
+    before = time()
+    movie = newlibrary.search("Cleopatra")
+    after = time()
+    time_taken = after - before
+    print("\t%s found in %fs" % (movie, time_taken))
+
+    before = time()
+    movie = newlibrary.search("Avatar 2")
+    after = time()
+    time_taken = after - before
+    print("\t%s found in %fs" % (movie, time_taken))
+
+    # newlibrary.bst._print_structure()
+    # print(newlibrary.bst._BSTproperties())
+
+
+if __name__ == "__main__":
+    main()
